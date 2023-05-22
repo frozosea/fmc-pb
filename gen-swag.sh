8cb6d3e12409f232fe95808ab6c605c09464f0cc
dizolo <y20072004@gmail.com>
2023-05-22 15:31:05 +0800

    fmc-pb: changes for v2.0.0(commercial)

---
+++ gen-swag.sh
@@ -0,0 +1,4 @@
+swagger-merger -i user/user.swagger.json -o user.swagger.yaml
+swagger-merger -i tracking/tracking.swagger.json -o tracking.swagger.yaml
+swagger-merger -i schedule-tracking/schedule_tracking.swagger.json -o schedule_tracking.swagger.yaml
+
